a0d64bcb81ffa70e1863b8bd4fceb9e8e5cf6dd8
kabilan-7 <murugankabilan39@gmail.com>
2024-10-02 18:21:39 +0530

    add chat-page ui

--- client/src/pages/chat/components/contacts-container/index.jsx
+++ client/src/pages/chat/components/contacts-container/index.jsx
@@ -7,6 +7,16 @@ const ContactsContainer = () => {
         <div className='pt-3'>
             <Logo/>
         </div>
+        <div className='my-5'>
+          <div className='flex items-center justify-between pr-10'>
+            <Title text = "Direct Messages"/>
+          </div>
+        </div>
+        <div className='my-5'>
+          <div className='flex items-center justify-between pr-10'>
+            <Title text = "Channels"/>
+          </div>
+        </div>
     </div>
   )
 }
@@ -46,4 +56,10 @@ const Logo = () => {
       </div>
     );
   };
+
+  const Title = ({text})=>{
+    return(
+    <h6 className="uppercase tracking-widest text-neutral-400 pl-10 font-light text-opacity-90">{text}</h6>
+    )
+  }
   
