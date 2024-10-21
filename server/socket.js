829a3329fc4dbcc0e2fd932640252d0092a67afb
kabilan-7 <murugankabilan39@gmail.com>
2024-10-21 21:53:13 +0530

    changes

--- server/socket.js
+++ server/socket.js
@@ -8,13 +8,24 @@ const setupSocket = (server) =>{
         credentials:true,
         }
     })
+    const disconnect = (socket) =>{
+        console.log(`Client discoonnected:${socket.id}`)
+        for(const [userId,socketId] of userSocketMap.entries()){
+            if(socketId == socket.id){
+            userSocketMap.delete(userId)
+            }
+        }
+    }
     const userSocketMap = new Map()
     io.on("connection",(socket)=>{
         const userId = socket.handshake.query.userId
         if(userId){
             userSocketMap.set(userId,socket.id)
             console.log(`User connected:${userId} with socket ID:${socket.id}`)
+        }else{
+            console.log("User id not provided during connection")
         }
+        socket.on("disconnect",()=> disconnect())
     })
 }
 
