e016d37af680ce5f47131ba1e598a042b8d1e295
kabilan-7 <murugankabilan39@gmail.com>
2024-11-30 20:58:44 +0530

    add renderMessage function

--- client/src/pages/chat/components/chat-container/components/message-container/index.jsx
+++ client/src/pages/chat/components/chat-container/components/message-container/index.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react'
 import { useAppStore } from '@/store'
+import moment from 'moment'
 const MessageContainer = () => {
   const scrollRef = useRef
   const {selectedChatData,selectedChatType,userInfo, selectedChatMessages} = useAppStore()
@@ -9,7 +10,23 @@ const MessageContainer = () => {
      }
   },[selectedChatMessages])
   const renderMessages = () => {
-
+       let lastDate = null;
+       return selectedChatMessages.map((message,index) =>{
+          const messageDate = moment(message.timestamp).format("YYYY-MM-DD");
+          const showDate = messageDate !== lastDate;
+          lastDate = messageDate;
+          return <div key={index}>
+            {
+              showDate && (
+                <div className='text-center text-gray-500 my-2'>{moment(timestamp).format("LL")}</div>
+              )
+            }
+            {
+              selectedChatType === 'contact' && renderDmMessages(message)
+            }
+          </div>
+       })
+       const renderDmMessages = (message) => <div className={`${message.sender !== selectedChatData ? "bg-[#8417ff]/5 text-[#8417ff]/90 border-[#ffffff]/20" : "bg-[#2a2b33]/5 text-white/80 border-[#8417ff]/50"} border inline-block p-4 rounded my-1 max-w-[50%] break-words`}></div>
   }
   return (
     <div className='flex-1 overflow-y-auto scrollbar-hidden p-4 px-8 md:w-[65vw] lg:w-[70vw] xl:w-[80vw] w-full '>
