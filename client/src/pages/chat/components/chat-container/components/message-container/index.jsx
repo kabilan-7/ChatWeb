fa4981ddccdd8f82e7faa83ff1724d51bcc6d454
kabilan-7 <murugankabilan39@gmail.com>
2024-12-01 20:29:13 +0530

    changes

--- client/src/pages/chat/components/chat-container/components/message-container/index.jsx
+++ client/src/pages/chat/components/chat-container/components/message-container/index.jsx
@@ -25,13 +25,18 @@ const MessageContainer = () => {
               selectedChatType === 'contact' && renderDmMessages(message)
             }
           </div>
-       })
-       const renderDmMessages = (message) => 
-        <div className={`${message.sender === selectedChatData.id ? "text-right" : "text-left"}`}>
-       <div className={`${message.sender !== selectedChatData ? "bg-[#8417ff]/5 text-[#8417ff]/90 border-[#ffffff]/20" : "bg-[#2a2b33]/5 text-white/80 border-[#8417ff]/50"} border inline-block p-4 rounded my-1 max-w-[50%] break-words`}>
-       </div>
-       </div>
-  }
+       });
+  };
+  const renderDmMessages = (message) => 
+    <div className={`${message.sender === selectedChatData.id ? "text-left" : "text-right"}`}>
+    {
+      message.messageType === "text" && (
+        <div className={`${message.sender !== selectedChatData ? "bg-[#8417ff]/5 text-[#8417ff]/90 border-[#ffffff]/20" : "bg-[#2a2b33]/5 text-white/80 border-[#8417ff]/50"} border inline-block p-4 rounded my-1 max-w-[50%] break-words`}>
+          {message.content}
+   </div>
+      )
+    }
+   </div>
   return (
     <div className='flex-1 overflow-y-auto scrollbar-hidden p-4 px-8 md:w-[65vw] lg:w-[70vw] xl:w-[80vw] w-full '>
       MessageContainer
